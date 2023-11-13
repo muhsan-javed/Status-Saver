3542120977e412fbc9947f0e1caf60db5a5a74d5
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-11-13 17:22:35 +0500

    add fragments

---
+++ app/src/main/java/com/muhsantech/statussaver/ui/fragments/SettingsFragment.kt
@@ -0,0 +1,33 @@
+package com.muhsantech.statussaver.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.muhsantech.statussaver.R
+import com.muhsantech.statussaver.databinding.FragmentSettingsBinding
+
+
+class SettingsFragment : Fragment() {
+    private lateinit var binding:FragmentSettingsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    companion object {
+    }
+}
